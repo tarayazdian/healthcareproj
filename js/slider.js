aedb30f44e52bc6edd07bba521963d473fc32fdc
tarayazdian <tara_yazdian@yahoo.com>
2021-01-23 20:45:11 +0330

    removed stylse used from theme and converted them to tailwind styles -update slider js file

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,16 @@
-let sliderElements = document.getElementsByClassName('blue-box');
-let sliderCounter = 0
+let sliderElements = document.getElementsByClassName("blue-box");
+let sliderCounter = 0;
 setInterval(() => {
-    for (let i = 0; i < sliderElements.length; i++) {
-        if (sliderElements[i].getAttribute('data-slide') == sliderCounter) {
-            sliderElements[i].classList.add('active');
-        }
-        else {
-            sliderElements[i].classList.remove('active');
-        }
+  for (let i = 0; i < sliderElements.length; i++) {
+    if (sliderElements[i].getAttribute("data-slide") == sliderCounter) {
+      sliderElements[i].classList.add("relative", "opacity-100");
+      sliderElements[i].classList.remove("absolute", "opacity-0");
+    } else {
+      sliderElements[i].classList.remove("relative", "opacity-100");
+      sliderElements[i].classList.add("absolute", "opacity-0");
     }
+  }
 
-    sliderCounter += 1;
-    sliderCounter %= 3
+  sliderCounter += 1;
+  sliderCounter %= 3;
 }, 3000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'black': '#000',
+      'white': '#fff',
+      'lightBlue': '#458ff6',
+      'darkBlue': '#5a98f2',
+      'gray': '#7d7987',
+      gradientBlue: 'linear-gradient(208.18deg, #67c3f3 9.05%, #5a98f2 76.74%)',
+    },
+    extend: {
+      backgroundImage: theme => ({
+       'servicesSection': "url('/images/Frame.png')",
+       'articleSection': "url('/images/element.png')",
+      })
+    }
   },
   variants: {
     extend: {},
